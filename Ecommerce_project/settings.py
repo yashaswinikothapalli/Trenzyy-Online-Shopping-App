fd3fc5b5e9c7ddaf64fbd1b95e0c77310e18e9cf
Yashaswini Kothapalli <yashaswinikothapalli@gmail.com>
2026-06-12 17:04:40 +0530

    adding products

--- Ecommerce_project/settings.py
+++ Ecommerce_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^dkh5p@li#kno=0@jg0ce+539k-^p%06=2j$i7hs6kpx@kwu0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
